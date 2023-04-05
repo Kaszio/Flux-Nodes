e9cc5f132cda5f6fb98fefde271d07034099b5a1
Kaszio <129131151+Kaszio@users.noreply.github.com>
2023-04-05 15:53:12 -0500

    Added  nodes + api node counter

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
-const imageWrapper = document.querySelector('.image-wrapper')
-const imageItems = document.querySelectorAll('.image-wrapper > *')
-const imageLength = imageItems.length
+const imageWrapper = document.querySelector('.image-wrapper');
+const imageItems = document.querySelectorAll('.image-wrapper > *');
+const imageLength = imageItems.length;
 const perView = 4;
 let totalScroll = 0;
 const delay = 3000;
 
-imageWrapper.style.setProperty('--per-view', perView)
+imageWrapper.style.setProperty('--per-view', perView);
 for(let i = 0; i < perView; i++) {
-  imageWrapper.insertAdjacentHTML('beforeend', imageItems[i].outerHTML)
+  imageWrapper.insertAdjacentHTML('beforeend', imageItems[i].outerHTML);
 }
 
-let autoScroll = setInterval(scrolling, delay)
+let autoScroll = setInterval(scrolling, delay);
 
 function scrolling() {
-  totalScroll++
+  totalScroll++;
   if(totalScroll == imageLength + 1) {
-    clearInterval(autoScroll)
-    totalScroll = 1
-    imageWrapper.style.transition = '0s'
-    imageWrapper.style.left = '0'
-    autoScroll = setInterval(scrolling, delay)
+    clearInterval(autoScroll);
+    totalScroll = 1;
+    imageWrapper.style.transition = '0s';
+    imageWrapper.style.left = '0';
+    autoScroll = setInterval(scrolling, delay);
   }
-  const widthEl = document.querySelector('.image-wrapper > :first-child').offsetWidth + 24
-  imageWrapper.style.left = `-${totalScroll * widthEl}px`
-  imageWrapper.style.transition = '.3s'
+  const widthEl = document.querySelector('.image-wrapper > :first-child').offsetWidth + 24;
+  imageWrapper.style.left = `-${totalScroll * widthEl}px`;
+  imageWrapper.style.transition = '.3s';
 }
+    
+// api url
+
+  const url = 'https://api.runonflux.io/daemon/getzelnodecount';
+
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let thedata = data;
+      let numofnodes = document.getElementById('danodes');
+      numofnodes.innerHTML = `${thedata.data.total}`;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
